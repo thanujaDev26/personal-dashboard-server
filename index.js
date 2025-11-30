cfdf093f3d2d6c837a4d7ce583f3bcb5ca6a3e6a
thanujadev26 <thanujapriyadarshane26@gmail.com>
2025-11-30 11:55:16 +0530

    V4 of vercel.json

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+const serverless = require('serverless-http');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/', (req, res) => {
     res.send('Hello World!');
-})
+});
+
 app.use('/leco', require('./routes/LecoRoutes'));
 
-
-// app.listen(port, () => {
-//     console.log(`Server running on http://localhost:${port}`);
-// });
-
-module.exports = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app);
