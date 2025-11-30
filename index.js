1db9bd55e2dccb2e61c8deb48732c2cd44b80a53
thanujadev26 <thanujapriyadarshane26@gmail.com>
2025-11-30 12:42:03 +0530

    Removed defualt get response

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.use('/leco', require('./routes/LecoRoutes'));
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+// app.listen(3001, '127.0.0.1', ()=>{
+//     console.log("Server is connected!")
+// })
